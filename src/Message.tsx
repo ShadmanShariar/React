e62c8fde77e1753d6733e24a00f1c6db6f7374a1
Shadman Shariar <75669228+ShadmanShariar@users.noreply.github.com>
2025-04-23 05:06:54 +0600

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+    let message = "Shadman";
+    if (message === "Shadman") {
+        return <h1>Hello Shadman</h1>;
+    }
+  return <h1>Hello World</h1>;
+}
+export default Message;
